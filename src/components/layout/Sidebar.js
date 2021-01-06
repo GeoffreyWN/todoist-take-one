e475d5031f42887d37390988a0784ee5b35110a9
geof <>
2021-01-06 17:21:21 +0300

    updated favicon, det :focus outline to none, fixed sidebar layout

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -11,48 +11,92 @@ import { Projects } from "../Projects";
 
 export const Sidebar = () => {
   const { setSelectedProject } = useSelectedProjectValue();
-  const [active, setActive] = useState('inbox');
+  const [active, setActive] = useState("inbox");
   const [showProjects, setShowProjects] = useState(true);
 
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li data-testid="inbox" className={active === 'inbox' ? 'active' : undefined} >
-          <div aria-label="Show Inbox Tasks" onClick={() => { setActive('inbox'); setSelectedProject('INBOX'); }} onKeyDown={() => { setActive('inbox'); setSelectedProject('INBOX'); }} tabIndex={0} role="button">
-            <span >
+        <li
+          data-testid="inbox"
+          className={active === "inbox" ? "active" : undefined}
+        >
+          <div
+            aria-label="Show Inbox Tasks"
+            onClick={() => {
+              setActive("inbox");
+              setSelectedProject("INBOX");
+            }}
+            onKeyDown={() => {
+              setActive("inbox");
+              setSelectedProject("INBOX");
+            }}
+            tabIndex={0}
+            role="button"
+          >
+            <span>
               <FaInbox />
             </span>
             <span>Inbox</span>
           </div>
-
         </li>
-        <li data-testid="today" className={active === 'today' ? 'active' : undefined} >
-          <div aria-label="Show Todays Task" onClick={() => { setActive('today'); setSelectedProject('TODAY'); }} onKeyDown={() => { setActive('today'); setSelectedProject('TODAY'); }} tabIndex={0} role="button">
-
-
+        <li
+          data-testid="today"
+          className={active === "today" ? "active" : undefined}
+        >
+          <div
+            aria-label="Show Todays Task"
+            onClick={() => {
+              setActive("today");
+              setSelectedProject("TODAY");
+            }}
+            onKeyDown={() => {
+              setActive("today");
+              setSelectedProject("TODAY");
+            }}
+            tabIndex={0}
+            role="button"
+          >
             <span>
               <FaRegCalendar />
             </span>
             <span>Today</span>
           </div>
         </li>
-        <li data-testid="next_7" className={active === 'next_7' ? 'active' : undefined}  >
-          <div aria-label="Show Tasks for the next 7 days" onClick={() => { setActive('next_7'); setSelectedProject('NEXT_7'); }} onKeyDown={() => { setActive('next_7'); setSelectedProject('NEXT_7'); }} tabIndex={0} role="button">
-
+        <li
+          data-testid="next_7"
+          className={active === "next_7" ? "active" : undefined}
+        >
+          <div
+            aria-label="Show Tasks for the next 7 days"
+            onClick={() => {
+              setActive("next_7");
+              setSelectedProject("NEXT_7");
+            }}
+            onKeyDown={() => {
+              setActive("next_7");
+              setSelectedProject("NEXT_7");
+            }}
+            tabIndex={0}
+            role="button"
+          >
             <span>
               <FaRegCalendarAlt />
             </span>
             <span>Next 7 days</span>
           </div>
         </li>
-
-
       </ul>
       <div className="sidebar__middle" aria-label="Show Hide Projects" onClick={() => setShowProjects(!showProjects)} onKeyDown={() => setShowProjects(!showProjects)} tabIndex={0} role="button">
         <span>
-          <FaChevronDown className={() => setSelectedProject(!showProjects)} /> <h2>Projects</h2>
+        <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          /> 
         </span>
+        <h2>Projects</h2>
       </div>
+
+      
       <ul className="sidebar__projects">{showProjects && <Projects />} </ul>
       {showProjects && <AddProject />}
 
